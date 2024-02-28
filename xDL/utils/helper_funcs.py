3d8dabef0df6d7b8eed18606d166faf986681b47
AFThielmann <antonthielmann@t-online.de>
2024-02-28 10:49:08 +0100

    delete old package folder

--- xDL/utils/helper_funcs.py
+++
@@ -1,8 +0,0 @@
-def flatten_list(input_list):
-    output_list = []
-    for item in input_list:
-        if isinstance(item, list):
-            output_list.extend(flatten_list(item))
-        else:
-            output_list.append(item)
-    return output_list
